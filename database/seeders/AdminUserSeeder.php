bbb5926a5751f6b763db6aad171c21f519c1bd0e
versionhash <parth.kanani@versionhash.com>
2021-04-09 13:59:56 +0530

    first commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //admin user
+        DB::table('users')->insert([
+            'name' => "SuperAdmin",
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
