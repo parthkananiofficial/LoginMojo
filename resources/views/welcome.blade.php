1553f03e763f47f77f314f9152896d0d5988b820
versionhash <parth.kanani@versionhash.com>
2021-04-17 09:02:13 +0530

    UI changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
       <div class="container">
         <nav class="navbar py-2 navbar-expand-lg flex justify-between items-center relative duration-300">
           <a class="navbar-brand">
-            <img src="{{ asset('img/logo.svg') }}" alt="Logo">
+            <img src="{{ asset('img/loginwithwhatsapp.png') }}" width="300" alt="Logo">
           </a>
           <button class="navbar-toggler focus:outline-none block lg:hidden" type="button" data-toggle="collapse"
             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -58,8 +58,8 @@
     <div class="container">
       <div class="flex justify-between">
         <div class="w-full text-center">
-          <h2 class="text-4xl font-bold leading-snug text-gray-700 mb-10 wow fadeInUp" data-wow-delay="1s">Startup and
-            SaaS Landing Page
+          <h2 class="text-4xl font-bold leading-snug text-gray-700 mb-10 wow fadeInUp" data-wow-delay="1s">
+              World's 1<sup>st</sup> WhatsApp Seamless Verification SaaS Platform
             <br class="hidden lg:block"> Built with TailwindCSS</h2>
           <div class="text-center mb-10 wow fadeInUp" data-wow-delay="1.2s">
 
@@ -541,7 +541,7 @@
         <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 wow fadeInUp" data-wow-delay="0.2s">
           <div class="mx-3 mb-8">
             <div class="footer-logo mb-3">
-              <img src="{{ asset('img/logo.svg') }}" alt="">
+              <img src="{{ asset('img/loginwithwhatsapp.png') }}" width="200" alt="">
             </div>
             <p class="text-gray-300">Developed with ♥ in India</p>
           </div>
